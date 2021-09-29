c5695f32f14a6f84ad7bd4e7484d42b562882dee
karthik <sec19it037@sairamtap.edu.in>
2021-09-29 16:08:28 +0530

    pdd app

--- app/src/main/java/com/example/diseasedetection/MainActivity.java
+++ app/src/main/java/com/example/diseasedetection/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.diseasedetection;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +14,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LinearLayout b1=findViewById(R.id.opencam);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, Disease.class);
+                startActivity(i);
+
+            }
+        });
+        LinearLayout b2=findViewById(R.id.pr);
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, Plantrecom.class);
+                startActivity(i);
+
+            }
+        });
     }
 }
